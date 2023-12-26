7afbc354b4ecf0af8f46746758a3e07a35686368
ScottForster400 <scottforster400@gmail.com>
2023-12-26 16:33:45 +0000

    main page css adedd

---
+++ aboutUs.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Cantor College||About Us</title>
+    <link rel="stylesheet" href="css/mobileMainPages.css">
+    <link rel="stylesheet" href="css/desktopMainPages.css" media="only screen and (min-width : 720px)">
+</head>
+<body>
+    <div class="container">
+        <?php
+            include("includes/nav-inc.php")
+        ?>
+        <main>
+            <section>
+                <div class="sidebar">
+                    <nav> 
+                        <menu>
+                            <a href="">About Us</a>
+                            <a href="">How to Find Us</a>
+                        </menu>
+                    </nav>
+                </div>
+                <div class="info">
+                    <h1>
+                        About Us
+                    </h1>
+                    <p>
+                        Cantor College was established in 1989 to specialize in Computing and Design.   At Cantor College, we want to give
+                        students the education they need to achieve their career aims, 
+                        leaving them equipped with the knowledge, skills and experience to succeed. Cantor College gives you the
+                        opportunities that can give you the edge when you enter the world of work,
+                        through our teaching and our links to some of the world's leading researchers and employers.
+                        Our students have gone on to successful careers in a wide range of industries across the globe.
+                        Whatever your ambitions, our learning and support can help to get the most out of your time with Cantor College, 
+                        both as a student and in your future career.
+                    </p>
+                    <h1>
+                        How to Find Us
+                    </h1>
+                    <div class="adress">
+                        <p>Cantor College</p>
+                        <p>Main Street</p>
+                        <p>Sheffield</p>
+                        <p>SC4 2BB</p>
+                        <p>Tel:(01321) 2340 235</p>
+                        <p>Fax: (01321) 2340 236</p>
+                        <P>Email: info@cantorcollege.ac.uk</p>
+                    </div>
+                    <div class="link-images">
+                            <div class="link-image">
+                                <a href="google.com"><img src="images/cantor-gallery.jpg" alt="courses"></a>
+                                <p>Contact Us</p>
+                            </div>
+                            
+                            
+                            <div class="link-image">
+                                <a href="google.com"><img src="images/cantor-lecture-theatre-3.jpg" alt="courses"></a>
+                                <p>Facilities</p>
+                            </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+        <?php
+            include("includes/footer-inc.php")
+        ?>
+    </div>
+    
+</body>
+</html>
--- includes/nav-inc.php
+++ includes/nav-inc.php
@@ -5,12 +5,12 @@
     </div>
     <nav>
         <menu>
-            <a href="">Home</a>
+            <a href="index.php">Home</a>
             <a href="">Courses</a>
             <a href="">Students</a>
             <a href="">Staff</a>
             <a href="">Bussness</a>
-            <a href="">About Us</a>       
+            <a href="aboutUs.php">About Us</a>       
         </menu>
     </nav>
 </header>
