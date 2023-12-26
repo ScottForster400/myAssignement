35569877ec0bd0c3ced11cf83817ab88e4906427
ScottForster400 <scottforster400@gmail.com>
2023-12-26 19:26:09 +0000

    students page added

--- includes/nav-inc.php
+++ includes/nav-inc.php
@@ -7,7 +7,7 @@
         <menu>
             <a href="index.php">Home</a>
             <a href="">Courses</a>
-            <a href="">Students</a>
+            <a href="students.php">Students</a>
             <a href="">Staff</a>
             <a href="">Bussness</a>
             <a href="aboutUs.php">About Us</a>       
---
+++ students.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Cantor College||About Us</title>
+    <link rel="stylesheet" href="css/mobileMainPages.css">
+    <link rel="stylesheet" href="css/desktopMainPages.css" media="only screen and (min-width : 720px)">
+</head>
+<body>
+    <div class="container">
+        <?php
+            include("includes/nav-inc.php")
+        ?>
+        <main>
+            <section>
+                <div class="sidebar">
+                    <nav> 
+                        <menu>
+                            <a href="">Academic Information</a>
+                            <a href="">Administrative Support</a>
+                            <a href="">Student Services</a>
+                            <a href="">University Services</a>
+                            <a href="">Life at University</a>
+                            <a href="">Communications</a>
+                        </menu>
+                    </nav>
+                </div>
+                <div class="info">
+                    
+                    <h1>Academic Information</h1>
+                    <ul>
+                        <li>Faculties - Links to all the Faculty and Departmental Offices with information on staff, courses and events.</li>
+                        <li>A to Z List of Departments - Departmental Homepages.</li>
+                        <li>Library and Learning Resource Services - Information on LLRS services, access to the on-line catalogue, links to external academic resources and guides 
+                                    to using IT to support your studies.</li>
+                        <li>Our Academic Partners - Information on College links.</li>
+                        <li>Research - links to the Research Centres, guide to research opportunities and help on finding research funding.</li>
+                    </ul>
+                    <h1>Administrative Support</h1>
+                    <ul>
+                        <li>Academic Registry - Information provided by the Academic Registry, including Academic Calendars, Academic Regulations, Module Catalogue, information on award ceremonies and tuition fees.</li>
+                        <li>Student Guide to Regulations - General and Academic Regulations, World Wide Web Code of Practice, Rules Governing the Use of LLRS facilities.</li>
+                    </ul>
+                    <h1>Student Services</h1>
+                    <ul>
+                        <li>Advisory and Counselling Centre - academic advice, help for international students, careers guidance, personal counselling, student loans and access funds and support for students with disabilities/learning difficulties.</li>
+                        <li>Health Centre - guide to the services provided by the University Health Centre.</li>
+                        <li>Multi-Faith Centre - information regarding the work of the Multi-Faith Centre.</li>
+                        <li>Pre-School Centre - contact information regarding the University's child care facilities.</li>  
+                        <li>Sports Centre - booking arrangements and facilities.</li>
+                    </ul>
+                    <h1>University Services</h1>
+                    <ul>
+                        <li>Catering Services - guide to the catering facilities on campus.</li>
+                        <li>Printing Services - information regarding services provided for students.</li>
+                        <li>Student Accommodation Services - guide to University Accommodation and help on finding accommodation in the private sector.</li>
+                    </ul>
+                    <h1>Life at University</h1>
+                    <ul>
+                        <li>Arts Centre - what's on at the University's arts venues.</li>
+                        <li>Students' Union - events and services provided by the Students' Union.</li>
+                        <li>University Guide - a full guide to life at the University.</li>
+                        <li>Campus Navigator - help with finding your way around campus.</li>
+                    </ul>
+                    <h1>Communications</h1>
+                    <ul>
+                        <li>Noticeboards - General, Classified, Events and Alumni noticeboards.</li>
+                        <li>Student Email Search - database of student email addresses.</li>
+                        <li>Staff Search - database of staff email addresses.</li>
+                        <li>A to Z List of Services - full A to Z list of University Services.</li>
+                    </ul>
+                    <div class="link-images">
+                            <div class="link-image">
+                                <a href="google.com"><img src="images/cantor-gallery.jpg" alt="courses"></a>
+                                <p>Contact Us</p>
+                            </div>
+                            
+                            
+                            <div class="link-image">
+                                <a href="google.com"><img src="images/cantor-lecture-theatre-3.jpg" alt="courses"></a>
+                                <p>Facilities</p>
+                            </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+        <?php
+            include("includes/footer-inc.php")
+        ?>
+    </div>
+    
+</body>
+</html>
